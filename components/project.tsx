0c2c2df586e3b607ccabe7bac0cdd43265579272
Jordan Sharpe <86000863+jsharpe27@users.noreply.github.com>
2023-09-16 11:49:19 -0400

    small change to project.tsx css

--- components/project.tsx
+++ components/project.tsx
@@ -24,12 +24,14 @@ export default function Project({title, description, tags, imageUrl}: ProjectPro
                     scale: scaleProgress,
                     opacity: opacityProgress
                 }}
-                className=' group mb-3 sm:mb-8 last:mb-0' //We use the group class to control the hover effect on the project component, and to control even or odd projects
+                className=' group 
+                mb-3 sm:mb-8 last:mb-0 
+            ' //We use the group class to control the hover effect on the project component, and to control even or odd projects
             >
                 <section 
                 className=' group bg-gray-100 max-w-[42rem] border
-                border-black/5 overflow-hidden sm:pr-8 
-                relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8
+                border-black/5 rounded-lg overflow-hidden sm:pr-8 
+                relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 group-even:pl-8
                 hover:bg-gray-200 transition'>
 
             <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 
